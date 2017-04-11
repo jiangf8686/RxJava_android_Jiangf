a20c4a0e9c2d2462186b11000e7a1cbcb57fe4f4
jiangfeng <feng.jiang@itel-mobile.com>
2017-04-11 18:16:12 +0800

    Initial commit

---
+++ app/src/main/java/com/rxjava/fengjiang/rxjava_android_jiangf/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rxjava.fengjiang.rxjava_android_jiangf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
